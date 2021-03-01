6db7eae9a3f3a638575f31f53fd532403fc0a735
Wellington Cruz <wellington.max1@gmail.com>
2021-02-28 22:47:25 -0300

    Iniciando projeto job de spring batch dfatura de cartão de crédito.

---
+++ src/main/java/com/springbatch/faturacartaodecredito/FaturaCartaoCreditoJobApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.faturacartaodecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaCartaoCreditoJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaCartaoCreditoJobApplication.class, args);
+	}
+
+}
