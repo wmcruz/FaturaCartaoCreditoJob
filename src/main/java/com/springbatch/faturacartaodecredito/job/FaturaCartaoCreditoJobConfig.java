6f68802e2d17b794e219832d763e44e1a8c55695
Wellington Cruz <wellington.max1@gmail.com>
2021-02-28 23:39:35 -0300

    Criação do job.

---
+++ src/main/java/com/springbatch/faturacartaodecredito/job/FaturaCartaoCreditoJobConfig.java
@@ -0,0 +1,26 @@
+package com.springbatch.faturacartaodecredito.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class FaturaCartaoCreditoJobConfig {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Bean
+	public Job faturaCartaoCreditoJob(Step faturaCartaoCreditoStep){
+		return this.jobBuilderFactory
+				.get("faturaCartaoCreditoJob")
+				.start(faturaCartaoCreditoStep)
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+ }
